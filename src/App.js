0beb383d6c8a647a729525c462b4f283e7384725
Mariano Mariano <marianofheller@gmail.com>
2018-06-29 23:32:36 -0300

    NavScroll

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
-import Tides from './components/Tides';
+import NavScroll from './components/NavScroll';
+import routes from './routes';
 
 const AppWrapper = styled.div`
   width: 100%;
@@ -11,9 +13,17 @@ const AppWrapper = styled.div`
 class App extends Component {
   render() {
     return (
-      <AppWrapper>
-        <Tides />
-      </AppWrapper>
+      <Router>
+        <AppWrapper>
+          <NavScroll />
+          <Switch>
+            {routes.map(routeConfig => <Route {...routeConfig} />)}
+            <Route render={() => {
+              return <Redirect to={routes[0].path} />
+            }}/>
+          </Switch>
+        </AppWrapper>
+      </Router>
     );
   }
 }
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import Tides from './components/Tides';
+
+const routes = [
+  {
+    path: '/tides',
+    title: 'Tides',
+    component: Tides,
+  }
+];
+
+
+export default routes;
