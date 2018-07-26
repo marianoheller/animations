71e851b1e5baf3169ec2fc65f7f48218c1e86db2
Mariano Heller <marianofheller@gmail.com>
2018-07-26 10:30:17 -0300

    showcase

--- src/components/Showcase/index.js
+++ src/components/Showcase/index.js
@@ -7,21 +7,32 @@ import routes from '../../routes';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 100%;
   height: 100%;
+  overflow: auto;
+  padding-bottom: 2rem;
+  background: linear-gradient( #ebd8eb, #ededd9);
 `;
 
 const Title = styled.div`
   width: 100%;
   text-align: center;
   font-size: 2rem;
+  color: #57014a;
   font-family: 'Galada', cursive;
 `;
 
 const AnimationContainers = styled.div`
-  margin: 3rem 25% 0 25%;
+  /* margin: 3rem 25% 0 25%; */
+  max-width: 600px;
   display: grid;
   grid-template-columns: repeat(2, 50%);
+
+  @media (max-width: 650px) {
+    max-width: 300px;
+    grid-template-columns: repeat(1, 100%);
+  }
 `;
 
 const Animation = styled.img`
